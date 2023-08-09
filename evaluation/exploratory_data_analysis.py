8ffd4730b6ccd7f6c0f34f1aa590b5183433d990
Tobias <fp83rusi@studserv.uni-leipzig.de>
2023-08-09 20:57:48 +0200

    add exploratory_data_analysis.py

---
+++ evaluation/exploratory_data_analysis.py
@@ -0,0 +1,77 @@
+from preprocessing.preprocessing import load_dataset
+from preprocessing.data_entry import Topic
+from config import Config
+
+from statistics import mean
+from typing import Dict, Union
+from pathlib import Path
+
+
+cfg = Config.get()
+
+
+def exploratory_data_analysis() -> Dict[int, Dict[str, Union[float, str]]]:
+    """
+    Create dictionary with exploratory-data-analysis statistics
+    :return: Dictionary with results per topic
+    """
+    dataset = load_dataset()
+    topic_ids = list(set(dataset["topic_id"].tolist()))
+
+    eda = dict()
+    for topic_id in topic_ids:
+        data = dataset[dataset["topic_id"] == topic_id]
+        elements = list()
+        for element in data["data"]:
+            elements.append(element)
+
+        average_amount_elements = list()
+        unique_words = list()
+        amount_empty_sets = 0
+        for element in elements:
+            average_amount_elements.append(len(element))
+            for word in element:
+                if word not in unique_words:
+                    unique_words.append(word)
+            if len(element) == 0:
+                amount_empty_sets += 1
+
+        average_amount_elements = mean(average_amount_elements)
+        amount_unique_words = len(unique_words)
+
+        topic_title = Topic.get(topic_number=topic_id).title
+        stats = {"title": topic_title, "average_amount_elements": average_amount_elements,
+                 "amount_empty_sets": amount_empty_sets, "amount_unique_words": amount_unique_words}
+
+        eda.setdefault(topic_id, stats)
+
+    return eda
+
+
+def create_eda_md_table(eda: Dict[int, Dict[str, Union[float, str]]]) -> str:
+    """
+    Create text for MD-File of exploratory data analysis
+    :param eda: Dictionary with exploratory data analysis
+    :return: String with MD-Table
+    """
+    text = "# Exploratory Data Analysis \n"
+    text += "| Topic-ID | Title | Average Amount Elements per Image | Amount Empty Sets | Amount Unique Words | \n"
+    text += "|---|---|---|---|---| \n"
+    for topic, stats in eda.items():
+        text += ("| " + str(topic) + " | " + str(stats["title"]) + " | " + str(stats["average_amount_elements"]) + " | "
+                 + str(stats["amount_empty_sets"]) + " | " + str(stats["amount_unique_words"]) + " | \n")
+
+    return text
+
+
+def run_exploratory_data_analysis():
+    """
+    Run exploratory data analysis and save analysis as MD-File
+    """
+    eda = exploratory_data_analysis()
+    text = [create_eda_md_table(eda=eda)]
+
+    # Save eda as MD-File in output_dir
+    with open(cfg.output_dir.joinpath(Path('exploratory_data_analysis.md')), 'w') as f:
+        for item in text:
+            f.write("%s\n" % item)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from preprocessing.preprocessing import create_dataset, load_dataset
+from evaluation.exploratory_data_analysis import run_exploratory_data_analysis
 
 """
 Main Method
@@ -6,7 +7,10 @@ Main Method
 
 # Create dataset: topic_ids [51, 100] or set topic_ids = None for all possible topics;
 # Specify size_dataset > 0 (images per topic)
-create_dataset(topic_ids=[51, 52], size_dataset=10)
+create_dataset(topic_ids=[51, 52, 53, 54, 55], size_dataset=50)
 
 # Load dataset as DataFrame for further use
 dataset = load_dataset()
+
+# Exploratory Data Analysis
+run_exploratory_data_analysis()
