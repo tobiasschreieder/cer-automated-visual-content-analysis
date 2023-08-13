140241d5243c34dbb06398a75cf1fdd3f97628a1
Tobias <fp83rusi@studserv.uni-leipzig.de>
2023-08-13 14:41:19 +0200

    add eda analysis images per topic

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Main Method
 
 # Create dataset: topic_ids [51, 100] or set topic_ids = None for all possible topics;
 # Specify size_dataset > 0 (images per topic)
-# create_dataset(topic_ids=[51, 52, 53, 54, 55], size_dataset=800)
+# create_dataset(size_dataset=800)
 
 # Load dataset as DataFrame for further use
 # dataset = load_dataset()
@@ -22,7 +22,7 @@ Main Method
 
 """
 svm = SVC()
-clf = Classifier(svm, dataset, 55)
+clf = Classifier(svm, dataset, topic_id=52)
 clf.fit()
 clf.evaluate()
 """
