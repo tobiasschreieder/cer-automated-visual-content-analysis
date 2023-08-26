12ad4214a5a14e91b463fc09e2054fc2bf0c87ae
Pinhio <j.philippzimmer@gmx.de>
2023-08-26 19:20:37 +0200

    pseudonymized

--- evaluation/dataset_evaluation.py
+++ evaluation/dataset_evaluation.py
@@ -16,22 +16,22 @@ def run_dataset_evaluation(file_1, file_2):
     out_path = cfg.output_dir
 
     # load evaluation files
-    labels_t = pd.read_csv(work_path.joinpath(file_1), sep=' ')
-    labels_p = pd.read_csv(work_path.joinpath(file_2), sep=' ')
+    labels_a = pd.read_csv(work_path.joinpath(file_1), sep=' ')
+    labels_b = pd.read_csv(work_path.joinpath(file_2), sep=' ')
 
-    labels_t.rename(columns={'Topic_correct': 'topic_correct_t'}, inplace=True)
-    labels_p.rename(columns={'Topic_correct': 'topic_correct_p'}, inplace=True)
+    labels_a.rename(columns={'Topic_correct': 'topic_correct_a'}, inplace=True)
+    labels_b.rename(columns={'Topic_correct': 'topic_correct_b'}, inplace=True)
 
     # merge files, eliminate unwanted records
-    labels = pd.merge(labels_t, labels_p, how='outer', on=['image_id']).dropna() \
+    labels = pd.merge(labels_a, labels_b, how='outer', on=['image_id']).dropna() \
         .drop_duplicates(subset='image_id', keep="first")
     
     # ensure typing
-    labels['topic_correct_p'] = labels['topic_correct_p'].astype(bool)
-    labels['topic_correct_t'] = labels['topic_correct_t'].astype(bool)
+    labels['topic_correct_b'] = labels['topic_correct_b'].astype(bool)
+    labels['topic_correct_a'] = labels['topic_correct_a'].astype(bool)
 
     # compute agreement between the annotators
-    labels['agree'] = (labels['topic_correct_t'] == labels['topic_correct_p'])
+    labels['agree'] = (labels['topic_correct_a'] == labels['topic_correct_b'])
 
     # create intercoder reliability DataFrame
     reliability = labels[['Topic_x', 'agree']].groupby(by='Topic_x').sum()
@@ -50,31 +50,31 @@ def run_dataset_evaluation(file_1, file_2):
     # compute Krippendorff's Alpha for each topic
     alphas = []
     for topic in labels['Topic_x'].unique():
-        reliability_data = labels.loc[labels['Topic_x'] == topic, ['topic_correct_p', 'topic_correct_t']].astype(int)
+        reliability_data = labels.loc[labels['Topic_x'] == topic, ['topic_correct_b', 'topic_correct_a']].astype(int)
         ka = krippendorff.alpha(reliability_data.transpose(), level_of_measurement='nominal')
         alphas.append(ka)
 
     # compute the overall alpha
-    alphas.append(krippendorff.alpha(labels[['topic_correct_p', 'topic_correct_t']].astype(int).transpose(), level_of_measurement='nominal'))
+    alphas.append(krippendorff.alpha(labels[['topic_correct_b', 'topic_correct_a']].astype(int).transpose(), level_of_measurement='nominal'))
 
     # add to reliability dataframe
     reliability['krippendorff'] = alphas
 
     # create topic validity DataFrame
     # merge annotators evaluations, clean DataFrame
-    validity_p = labels[['Topic_y', 'topic_correct_p']]
-    validity_t = labels[['Topic_x', 'topic_correct_t']]
-    validity = pd.merge(validity_p, validity_t, left_index=True, right_index=True) \
+    validity_b = labels[['Topic_y', 'topic_correct_b']]
+    validity_a = labels[['Topic_x', 'topic_correct_a']]
+    validity = pd.merge(validity_b, validity_a, left_index=True, right_index=True) \
         .drop(columns='Topic_y').rename(columns={'Topic_x': 'topic'})
     
     # add column for cases where both annotated true
-    validity['topic_correct_both'] = validity['topic_correct_p'] & validity['topic_correct_t']
+    validity['topic_correct_both'] = validity['topic_correct_b'] & validity['topic_correct_a']
 
     # sum up by topic and create new columns
     validity = validity.groupby(by='topic').sum()
     validity['size'] = sizes
-    validity['percent_correct_p'] = (validity['topic_correct_p'] / validity['size'])
-    validity['percent_correct_t'] = (validity['topic_correct_t'] / validity['size'])
+    validity['percent_correct_b'] = (validity['topic_correct_b'] / validity['size'])
+    validity['percent_correct_a'] = (validity['topic_correct_a'] / validity['size'])
     validity['percent_correct_both'] = (validity['topic_correct_both'] / validity['size'])
 
     validity = validity.reset_index()
