b4a7acdecc0f6ba677c3237a13d996e0be93a40d
Pinhio <j.philippzimmer@gmx.de>
2023-08-26 19:24:39 +0200

    able to run everything now

--- main.py
+++ main.py
@@ -1,39 +1,69 @@
-from preprocessing.preprocessing import create_dataset, load_dataset, create_clarifai_dataset, load_clarifai_dataset
+from preprocessing.preprocessing import create_dataset, load_dataset #, create_clarifai_dataset, load_clarifai_dataset
 from evaluation.exploratory_data_analysis import run_exploratory_data_analysis
 from evaluation.dataset_evaluation import run_dataset_evaluation, print_dataset_evaluation
-# from computer_vision.computer_vision import run_clarifai
+from computer_vision.computer_vision import run_clarifai
 from classification.classification import Classifier
-from sklearn.svm import SVC
 
-from classification.models import svm_test
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import PassiveAggressiveClassifier
+
+import json
 
 """
 Main Method
 """
 
-# Create dataset: topic_ids [51, 100] or set topic_ids = None for all possible topics;
-# Specify size_dataset > 0 (images per topic)
-
-# create_dataset(size_dataset=800)
-
-# Load dataset as DataFrame for further use
-# dataset = load_dataset()
-
 # Exploratory Data Analysis
-# run_exploratory_data_analysis()
+run_exploratory_data_analysis()
 
-# run_clarifai(image_ids=["I00000ed2c519e6b9710fd6ad", "I000a29953c54d0b4380724fa"])
+# evaluation of data set coding
+run_dataset_evaluation('image_eval_t.txt', 'image_eval_p.txt')
+print_dataset_evaluation()
 
-#svm = SVC()
-#clf = Classifier(svm, dataset, 51)
-#clf.fit()
-#clf.evaluate()
 
-#run_dataset_evaluation('image_eval_t.txt', 'image_eval_p.txt')
-#print_dataset_evaluation()
+with open('classification/models.json', 'r') as f:
+    models = json.load(f)
+data = {
+    # clariafai
+    'dataset_clarifai_topics=55+76+81_size=900.pkl': load_dataset(size_dataset=900, topic_ids=[55,76,81], use_clarifai_data=True),
 
-svm = SVC()
-grid_params = svm_test
-dataset = load_dataset()
-clf = Classifier(svm, dataset, 51, grid_params=grid_params)
-clf.evaluate_grid_search()
+    # combined
+    'dataset_combined_topics=55+76+81_size=900.pkl': load_dataset(size_dataset=900, topic_ids=[55,76,81], use_combined_dataset=True),
+
+    # google
+    'dataset_touche_topics=55+76+81_size=900.pkl': load_dataset(size_dataset=900, topic_ids=[55,76,81]),
+    'dataset_touche_topics=51+55+76+81+100_size=900.pkl': load_dataset(size_dataset=900,  topic_ids=[51,55,76,81,100])
+}
+
+
+for key in models.keys():
+    for file in data.keys():
+
+        topics = data[file]['topic_id'].unique()
+
+        if key == 'svm':
+            model = SVC()
+        elif key == 'gbc':
+            model = GradientBoostingClassifier()
+        elif key == "sgd":
+            model = SGDClassifier()
+        elif key == 'pac':
+            model = PassiveAggressiveClassifier()
+
+        grid_params = models[key]
+
+        for topic in topics:
+
+            print(f'Start performing BINARY grid search for {file}\nTopic: {topic} out of {list(topics)}\nClassifier: {str(model)}')
+            # don't use likelihood
+            clf = Classifier(model, data[file], topic, grid_params=grid_params, dataset_name=file, use_likelihood=False)
+            clf.evaluate_grid_search()
+
+            print(f'Start performing LIKELIHOOD grid search for {file}\nTopic: {topic} out of {list(topics)}\nClassifier: {str(model)}')
+            # use likelihood
+            clf = Classifier(model, data[file], topic, grid_params=grid_params, dataset_name=file, use_likelihood=True)
+            clf.evaluate_grid_search()
+
+# The results can be evaluated with evaluation_of_result.ipynb in /evaluation
